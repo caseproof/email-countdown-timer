15327abbdf8d734c20d81983bed389a3602a7893
Blair Williams <blair@caseproof.com>
2019-06-05 11:38:53 -0600

    Added no cache headers so email client cant cache it

--- index.php
+++ index.php
@@ -267,6 +267,11 @@ class CountdownTimer
   }
 }
 
+header('Content-Type: image/gif');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 /**
  * Create a new countdown
  */
